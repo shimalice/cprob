4ed33f422b7558725f74b2dcf5df74c289adae39
shimalice <shimalicelis@gmail.com>
2017-11-01 20:57:17 +0900

    deleted not ch01 files

--- ch01/array_by_macro.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define ASIZE 1000;
-
-int main(){
-  int asize = 100;
-  int array[asize];
-}
-
--- ch01/macro.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#define GRAV 9.80665
-int main(){
-  printf("g = %f\n", GRAV);
-}
