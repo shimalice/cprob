c89e454212d0429a01ec0588af36801f6675f91d
shimalice <shimalicelis@gmail.com>
2017-11-14 15:24:00 +0900

    new prob16

--- ch02/prob10.c
+++ ch02/prob10.c
@@ -7,12 +7,12 @@ int main(){
   printf("整数値を10個入力してください．一つ入力するごと改行を押してください．\n");
   for (int count1 = 0; count1 < 10; count1++) {
     scanf("%d", p);
-    p ++;
+    p++;
   }
   p = q;
   printf("中身は以下の通りです．\n");
   for (int count2 = 0; count2 < 10; count2++) {
     printf("値:%d アドレス:%p\n", *p, p);
-    p ++;
+    p++;
   }
 }
---
+++ ch03/prob16.c
@@ -0,0 +1,51 @@
+// prob16
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct person {
+	char name[30];
+	char address[40];
+	int birthyear;
+} personData;
+
+typedef struct company {
+  personData ceo;
+  personData employees[100];
+} companyData;
+
+int main(){
+  companyData  *companies, *addressbook;
+  companies = (companyData*)malloc(sizeof(companyData)*3);
+  addressbook = companies;
+  // personData ceo;
+  // ceo = (personData*)malloc(sizeof(personData));
+  for(int i = 0; i < 3; i++){
+    personData *ceo;
+    ceo = (personData*)malloc(sizeof(personData));
+    printf("What is the name of ceo of the company%d?\n", i);
+    scanf("%s", ceo->name);
+    printf("What is the address of %s?\n", ceo->name);
+    scanf("%s", ceo->address);
+    companies->ceo = *ceo;
+    personData *employees;
+    employees = (personData*)malloc(sizeof(personData)*3);
+    for(int j = 0; j < 3; j++){
+      printf("What is the employee%d's name of the companie%d ?\n", j, i);
+      scanf("%s", employees->name);
+      companies->employees[j] = *employees;
+      employees++;
+    }
+    companies++;
+  }
+
+
+  printf("\n");
+  companies = addressbook;
+  for(int i = 0; i < 3; i++){
+    printf("The ceo of the company%d: %s, ", i, companies->ceo.name);
+    printf("lives in %s\n", companies->ceo.address);
+    for(int j = 0; j < 3; j++){
+      printf("the employee%d's name of the companie%d: %s \n", j, i, companies->employees[j].name);
+    }
+    companies++;
+  }
+}
